1d71d018d7ce474c4f96d920a49c329bf8575b28
Toby Flynn <toby_flynn@btinternet.com>
2025-09-10 10:19:09 +0100

    Adding support for prisms to span averager

--- pyfr/writers/vtk/spanavg.py
+++ pyfr/writers/vtk/spanavg.py
@@ -12,8 +12,9 @@ class VTKSpanAvgWriter(BaseVTKWriter):
     output_curved = True
     output_partition = False
 
-    def _load_soln(self, *args, **kwargs):
-        super()._load_soln(*args, **kwargs)
+    def _load_hex(self):
+        if 'hex' not in self.mesh.spts:
+            return None, None
 
         nspts, neles = self.mesh.spts['hex'].shape[:2]
 
@@ -107,6 +108,116 @@ class VTKSpanAvgWriter(BaseVTKWriter):
         # Reorder the axes in the original way for the mesh
         mesh2d = mesh2d.swapaxes(0, 1)
 
+        return mesh2d, soln2d, curved
+    
+    def _load_pri(self):
+        if 'pri' not in self.mesh.spts:
+            return None, None
+
+        nspts, neles = self.mesh.spts['pri'].shape[:2]
+
+        # 2D number of shape pts        
+        if nspts == 6:
+            nspts2d = 3
+        elif nspts == 18:
+            nspts2d = 6
+        else:
+            raise RuntimeError('Unrecognised nspts')
+        
+        # Get Quad rules for hex elements
+        shapecls = subclass_where(BaseShape, name='pri')
+        shape = shapecls(nspts, self.cfg)
+        order = self.cfg.getint('solver', 'order')
+        # Get physical location of each soln point
+        ploc = shape.sbasis.nodal_basis_at(shape.upts).astype(self.dtype) @ self.mesh.spts['pri'].reshape(nspts, -1)
+        ploc = ploc.reshape(shape.nupts, -1, self.ndims)
+        ploc = np.swapaxes(ploc, 0, 1)
+        # Get weights at each soln point (but the line shape weights as we're integrating along separate lines, not volumes)
+        if self.cfg.get(f'solver-elements-{shape.name}', 'soln-pts') != 'williams-shunn~gauss-legendre':
+            raise Exception('Span averaging on prisms only supports the williams-shunn~gauss-legendre solution points')
+        line = get_quadrule('line', 'gauss-legendre', order + 1)
+        # Get number of tri solution points
+        trinupts = (order + 1)*((order + 1) + 1) // 2
+        # Swap axis of soln to match ploc
+        soln = self.soln['pri'].swapaxes(0, 1).swapaxes(0, 2)
+        # Get new arrays
+        nvals = soln.shape[2]
+        ploc2d = np.zeros((neles, trinupts, 2), dtype=self.dtype)
+        soln2d = np.zeros((neles, trinupts, nvals), dtype=self.dtype)
+        mesh2d = np.zeros((neles, nspts2d, 2), dtype=self.dtype)
+        curved = self.mesh.spts_curved['pri']
+        # Iterate over each element and reduce to 2D quad
+        for _ploc, _soln, _mesh, _ploc2d, _soln2d, _mesh2d in zip(ploc, soln, self.mesh.spts['pri'].swapaxes(0, 1), ploc2d, soln2d, mesh2d):
+            # TODO will this always be the case for shape points?
+            for i in range(0, nspts2d):
+                _mesh2d[i][0] = _mesh[i][0]
+                _mesh2d[i][1] = _mesh[i][1]
+            # Integrate within element to spanwise average within an element
+            for i in range(0, trinupts):
+                _ploc2d[i][0] = _ploc[i][0]
+                _ploc2d[i][1] = _ploc[i][1]
+                for v in range(0, nvals):
+                    for j in range(0, order + 1):
+                        _soln2d[i][v] += _soln[i + j * trinupts][v] * line.wts[j]
+                    # Line weights integrate [-1,1], so need to divide by 2
+                    _soln2d[i][v] *= 0.5
+        # Reduce stacks of 2D tris to a single tri
+        # *** This assumes evenly spaced Z dimension layers in the mesh!!! ***
+        elements2d = {}
+        centreAvgDP = 6
+        ploc2dTol = 1e-6
+        for _ploc2d, _soln2d, _mesh2d, _curved in zip(ploc2d, soln2d, mesh2d, curved):
+            # Key is average point rounded to 6 decimal places 
+            plockey = (np.round(np.sum(_ploc2d[:,0]) / len(_ploc2d), centreAvgDP), np.round(np.sum(_ploc2d[:,1]) / len(_ploc2d), centreAvgDP))
+            if plockey in elements2d:
+                acc = elements2d[plockey]
+                for ptInd in range(0, len(_ploc2d)):
+                    found = False
+                    for accInd in range(0, len(_ploc2d)):
+                        if abs(_ploc2d[ptInd,0] - acc[0][accInd,0]) < ploc2dTol and abs(_ploc2d[ptInd,1] - acc[0][accInd,1]) < ploc2dTol:
+                            acc[1][accInd] += _soln2d[ptInd]
+                            found = True
+                            break
+                    if not found:
+                        print('Did not find a matching solution point!')
+                        print(f'Point: {_ploc2d[ptInd]}')
+                        print(f'Point key: {plockey}')
+                        print(acc[0])
+                        print('pri')
+                        print('***')
+                acc[2] += 1
+                if _curved:
+                    acc[4] += 1
+            else:
+                acc = [_ploc2d, _soln2d, 1, _mesh2d, 1 if _curved else 0]
+            elements2d[plockey] = acc
+        # Build new arrays
+        nelem2d = len(elements2d)
+        ploc2d = np.zeros((nelem2d, trinupts, 2), dtype=self.dtype)
+        soln2d = np.zeros((nelem2d, trinupts, nvals), dtype=self.dtype)
+        mesh2d = np.zeros((nelem2d, nspts2d, 2), dtype=self.dtype)
+        curved = []
+        elemind = 0
+        for _plockey in elements2d:
+            ploc2d[elemind] = elements2d[_plockey][0]
+            soln2d[elemind] = elements2d[_plockey][1] / self.dtype(elements2d[_plockey][2])
+            mesh2d[elemind] = elements2d[_plockey][3]
+            curved.append(elements2d[_plockey][4] != 0)
+            elemind += 1
+        # Reorder axes in the original way for solution
+        soln2d = soln2d.swapaxes(0, 1).swapaxes(1, 2)
+        # Reorder the axes in the original way for the mesh
+        mesh2d = mesh2d.swapaxes(0, 1)
+
+        return mesh2d, soln2d, curved
+
+    def _load_soln(self, *args, **kwargs):
+        super()._load_soln(*args, **kwargs)
+
+        hexmesh2d, hexsoln2d, hexcurved = self._load_hex()
+
+        primesh2d, prisoln2d, pricurved = self._load_pri()
+
         class SpanMeshAdapter:
             etypes = []
             eidxs = {}
@@ -115,18 +226,37 @@ class VTKSpanAvgWriter(BaseVTKWriter):
 
         _mesh2d = SpanMeshAdapter()
 
-        _mesh2d.etypes = ['quad']
-        _mesh2d.eidxs = {'quad': np.array([i for i in range(0, mesh2d.shape[1])])}
-        _mesh2d.spts = {'quad': mesh2d}
-        _mesh2d.spts_curved = {'quad': curved}
+        if hexmesh2d:
+            _mesh2d.etypes.append('quad')
+            _mesh2d.eidxs['quad'] = np.array([i for i in range(0, hexmesh2d.shape[1])])
+            _mesh2d.spts['quad'] = hexmesh2d
+            _mesh2d.spts_curved['quad'] = hexcurved
+        
+        if primesh2d:
+            _mesh2d.etypes.append('pri')
+            _mesh2d.eidxs['pri'] = np.array([i for i in range(0, primesh2d.shape[1])])
+            _mesh2d.spts['pri'] = primesh2d
+            _mesh2d.spts_curved['pri'] = pricurved
+
         self.mesh = _mesh2d
 
-        self.soln['quad'] = soln2d
-        self.soln['quad-parts'] = np.ones((mesh2d.shape[1]))
+        if hexsoln2d:
+            self.soln['quad'] = hexsoln2d
+            self.soln['quad-parts'] = np.ones((hexmesh2d.shape[1]))
+        
+        if prisoln2d:
+            self.soln['pri'] = prisoln2d
+            self.soln['pri-parts'] = np.ones((primesh2d.shape[1]))
 
         # Update cfg for 2D
-        self.cfg.set('solver-elements-quad', 'soln-pts', self.cfg.get(f'solver-elements-{shape.name}', 'soln-pts'))
-        self.cfg.rename_section('solver-elements-hex', 'z')
+        if hexmesh2d:
+            self.cfg.set('solver-elements-quad', 'soln-pts', self.cfg.get(f'solver-elements-{shape.name}', 'soln-pts'))
+            self.cfg.rename_section('solver-elements-hex', 'z1')
+        
+        if primesh2d:
+            self.cfg.set('solver-elements-tri', 'soln-pts', 'williams-shunn')
+            self.cfg.rename_section('solver-elements-pri', 'z2')
+
         self.ndims = 2
 
         self.einfo = [(etype, self.soln[etype].shape[2])
